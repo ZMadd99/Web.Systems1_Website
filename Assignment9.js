20d5a41ae9066d536759d9e539cfa1ac1795c2fa
ZMadd99 <143984259+ZMadd99@users.noreply.github.com>
2023-10-27 18:41:20 -0400

    Update Assignment9.js

--- Assignment9.js
+++ Assignment9.js
@@ -1,11 +1,9 @@
 const canvas = document.getElementById("animationCanvas");
 const ctx = canvas.getContext("2d");
 
-// Set canvas size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Object properties
 const objectSize = 50;
 let x = 50; // Initial X position
 let y = 50; // Initial Y position
@@ -16,18 +14,14 @@ let ySpeed = 2; // Initial Y speed
 function animate() {
   requestAnimationFrame(animate);
 
-  // Clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw the object
   ctx.fillStyle = "blue";
   ctx.fillRect(x, y, objectSize, objectSize);
 
-  // Update object position
   x += xSpeed;
   y += ySpeed;
 
-  // Bounce when hitting the canvas boundaries
   if (x + objectSize > canvas.width || x < 0) {
     xSpeed = -xSpeed;
   }
@@ -36,23 +30,17 @@ function animate() {
   }
 }
 
-// Start the animation
 animate();
 
-// Handle mouse click event
 canvas.addEventListener("click", () => {
-  // Change the object's color on mouse click
   ctx.fillStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
 });
 
 canvas.addEventListener("click", () => {
-    // Change the object's color on mouse click
     ctx.fillStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
   
-    // Change the object's direction on mouse click
     xSpeed = Math.random() > 0.5 ? Math.abs(xSpeed) : -Math.abs(xSpeed);
     ySpeed = Math.random() > 0.5 ? Math.abs(ySpeed) : -Math.abs(ySpeed);
   
-    // Add more interactions as desired
   });
   
